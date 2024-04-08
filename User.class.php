da87802e3de73bc6f311f803f17249c41129ed68
1400stachu <stasiek.jurski@gmail.com>
2024-04-08 13:19:04 +0200

    ssss

--- User.class.php
+++ User.class.php
@@ -1,28 +1,55 @@
 <?php
 class User {
-
+  
     private $id;
     private $email;
     private $password;
 
+   
 
-    public static function Register(string $email, string $password) {
     
-    $dh - new mysqli('localhost', 'root', '', 'cms')
-    $q = $db->prepare("INSERT INTO user VALUES (NULL, ?, ?)");
-    $passwordHash = password_hash($password, PASSWORD_ARGON2I);
-    $q->bind_param("ss", $email, $passwordHash);
-    $result = $q->execute();
-
-
+    public function __construct(int $id, string $email)
+    {
+      
+        $this->id = $id;
+        $this->email = $email;
     }
-    public static function Login(string $email, string $password) {
 
 
+    public static function Register(string $email, string $password) : bool {
+      
+        $db = new mysqli('localhost', 'root', '', 'cms');
+        $sql = "INSERT INTO user (email, password) VALUES (?, ?)";
+        $q = $db->prepare($sql);
+        $passwordHash = password_hash($password, PASSWORD_ARGON2I);
+        $q->bind_param("ss", $email, $passwordHash);
+        $result = $q->execute();
+        return $result;
+    }
+    public static function Login(string $email, string $password) : bool {
+       
+        $db = new mysqli('localhost', 'root', '', 'cms');
+        $sql = "SELECT * FROM user WHERE email = ? LIMIT 1";
+        $q = $db->prepare($sql);
+        $q->bind_param("s", $email);
+        $q->execute();
+        $result = $q->get_result();
+        $row = $result->fetch_assoc();
+        
+        $id = $row['ID'];
+        $passwordHash = $row['password'];
+        if(password_verify($password, $passwordHash)) {
+            
+            $user = new User($id, $email);
+            $_SESSION['user'] = $user;
+            return true;
+        } else {
+           
+            return false;
+        }
     }
     public function Logout() {
-    
+       
 
     }
 }
-?>
